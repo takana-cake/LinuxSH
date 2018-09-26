9ca5b401b3976fe117fa062b8a5fa3ec706d033d
takana303 <msdf303@live.jp>
2018-09-27 00:42:22 +0900

    Update traffic.sh

--- traffic.sh
+++ traffic.sh
@@ -1,41 +1,57 @@
 #!/bin/sh
 
-date +"%Y/%m/%d %H:%M" >> /var/log/daylog_`date +\%Y\%m\%d`.log
-
-echo "### uptime ###" >> /var/log/daylog_`date +\%Y\%m\%d`.log
-
-DAY=`uptime | tr -s [:space:] | sed -e 's/,//g' | cut -d " " -f 4`
-TIME=`uptime | tr -s [:space:] | sed -e 's/,//g' | cut -d " " -f 6`
-MIN=`uptime | tr -s [:space:] | sed -e 's/,//g' | cut -d " " -f 7`
-if [ MIN = "min" ]
-then
-TIME="00:${MIN}"
-fi
-echo "${DAY} Days ${TIME}" >> /var/log/daylog_`date +\%Y\%m\%d`.log
+LOGFILE="/var/log/daylog.log"
+DATE=`date +\%Y\%m\%d`
+ECHO="${DATE}"
 
 
-echo "### network ###" >> /var/log/daylog_`date +\%Y\%m\%d`.log
+UPTIME_DAY=$(( `uptime | tr -s [:space:] | sed -e 's/,//g' | cut -d " " -f 4` ))
+ECHO="${ECHO}: UPTIME ${UPTIME_DAY} Days"
+
+
 RX=`cat /proc/net/dev | sed -n 4p | tr -s [:space:] | cut -d " " -f 3`
 TX=`cat /proc/net/dev | sed -n 4p | tr -s [:space:] | cut -d " " -f 11`
 RXATX=$(( RX + TX ))
 GB=1073741824
 
 COUNT=0
-QUOTIENT=0
-QUOTIENT_=0
+TOTAL_QUOTIENT=0
+TOTAL_DECIMAL=0
 REMAINDER=0
 
-QUOTIENT=$(( RXATX / GB ))
+TOTAL_QUOTIENT=$(( RXATX / GB ))
 REMAINDER=$(( RXATX % GB * 10 ))
 while [ $REMAINDER -ne 0 -a $COUNT -lt 2 ]
 do
-	QUOTIENT_=$(( QUOTIENT_ * 10 + REMAINDER / GB ))
+	TOTAL_DECIMAL=$(( TOTAL_DECIMAL * 10 + REMAINDER / GB ))
 	REMAINDER=$(( REMAINDER % GB * 10 ))
 	COUNT=$(( COUNT + 1 ))
 done
 
-echo "${QUOTIENT}.${QUOTIENT_} GB" >> /var/log/daylog_`date +\%Y\%m\%d`.log
 
-#日でわるしょりを入れる。再起動したときのことを考える
-#増えたログを消す処理を入れる
+YESTERDAY_UPTIME=$(( `tail -n 1 /var/log/daylog.log | cut -d " " -f 3` ))
+YESTERDAY_QUOTIENT=$(( `tail -n 1 /var/log/daylog.log | cut -d " " -f 6 | cut -d "." -f 1` ))
+YESTERDAY_DECIMAL=$(( `tail -n 1 /var/log/daylog.log | cut -d " " -f 6 | cut -d "." -f 2` ))
+if [ $((YESTERDAY_UPTIME)) -gt $((UPTIME_DAY)) ]; then
+	TOTAL_tmp=$(( TOTAL_QUOTIENT * 100 + TOTAL_DECIMAL ))
+	YESTERDAY_tmp=$(( YESTERDAY_QUOTIENT * 100 + YESTERDAY_DECIMAL ))
+	TOTAL_tmp=$(( TOTAL_tmp + YESTERDAY_tmp ))
+	TOTAL_QUOTIENT=$(( TOTAL_tmp / 100 ))
+	TOTAL_DECIMAL=$(( TOTAL_tmp % 100 ))
+fi
+ECHO="${ECHO}: TOTAL ${TOTAL_QUOTIENT}.${TOTAL_DECIMAL} GB"
 
+
+COUNT=0
+TODAY_TMP=$(( ( TOTAL_QUOTIENT * 100 + TOTAL_DECIMAL ) - ( YESTERDAY_QUOTIENT * 100 + YESTERDAY_DECIMAL ) ))
+TODAY_QUOTIENT=$(( TODAY_TMP / 100 ))
+REMAINDER=$(( TODAY_TMP % 100 * 10 ))
+while [ $COUNT -lt 2 ]
+do
+	TODAY_DECIMAL=$(( TODAY_DECIMAL * 10 + REMAINDER / 100 ))
+	REMAINDER=$(( REMAINDER % 100 * 10 ))
+	COUNT=$(( COUNT + 1 ))
+done
+ECHO="${ECHO}: TODAY ${TODAY_QUOTIENT}.${TODAY_DECIMAL} GB"
+
+echo $ECHO >> $LOGFILE
