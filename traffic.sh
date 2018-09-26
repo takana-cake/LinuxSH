6625d1e0fac6fe8ce029478de84b449945c83783
takana303 <msdf303@live.jp>
2018-09-27 01:00:54 +0900

    Update traffic.sh

--- traffic.sh
+++ traffic.sh
@@ -4,11 +4,17 @@ LOGFILE="/var/log/daylog.log"
 DATE=`date +\%Y\%m\%d`
 ECHO="${DATE}"
 
+# リセット日
+RESETDATE=20
+if [ $(( `date +\%-d` )) -eq "27" ]; then
+	echo "${DATE} :reset 0 0 0 0.0"
+fi
 
+# uptime取得
 UPTIME_DAY=$(( `uptime | tr -s [:space:] | sed -e 's/,//g' | cut -d " " -f 4` ))
 ECHO="${ECHO}: UPTIME ${UPTIME_DAY} Days"
 
-
+# RX+TXをGBへ
 RX=`cat /proc/net/dev | sed -n 4p | tr -s [:space:] | cut -d " " -f 3`
 TX=`cat /proc/net/dev | sed -n 4p | tr -s [:space:] | cut -d " " -f 11`
 RXATX=$(( RX + TX ))
@@ -28,7 +34,8 @@ do
 	COUNT=$(( COUNT + 1 ))
 done
 
-
+# TOTALTRAFFICを取得
+# 再起動起きてたら前日のTRAFFICを足す
 YESTERDAY_UPTIME=$(( `tail -n 1 /var/log/daylog.log | cut -d " " -f 3` ))
 YESTERDAY_QUOTIENT=$(( `tail -n 1 /var/log/daylog.log | cut -d " " -f 6 | cut -d "." -f 1` ))
 YESTERDAY_DECIMAL=$(( `tail -n 1 /var/log/daylog.log | cut -d " " -f 6 | cut -d "." -f 2` ))
@@ -41,7 +48,7 @@ if [ $((YESTERDAY_UPTIME)) -gt $((UPTIME_DAY)) ]; then
 fi
 ECHO="${ECHO}: TOTAL ${TOTAL_QUOTIENT}.${TOTAL_DECIMAL} GB"
 
-
+# 前日のTOTALTRAFFICからTRAFFIC/dayを取得
 COUNT=0
 TODAY_TMP=$(( ( TOTAL_QUOTIENT * 100 + TOTAL_DECIMAL ) - ( YESTERDAY_QUOTIENT * 100 + YESTERDAY_DECIMAL ) ))
 TODAY_QUOTIENT=$(( TODAY_TMP / 100 ))
