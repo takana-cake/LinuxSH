be2a7865809d45321902bd7174cc30d0832573a8
takana303 <msdf303@live.jp>
2018-10-30 06:00:05 +0900

    Update usb_mount.sh

--- usb_mount.sh
+++ usb_mount.sh
@@ -8,11 +8,19 @@
 #
 #swatchかなんかで監視して実行？
 
+#ls /dev/disk/by-id/ -l | sed -e "1d" | tr -s [:space:] | cut -d " " -f 9 >> ./usb_mount_exception.list
+EXCEPTION=`cat ./usb_mount_exception.list`
 
-EXCEPTION=`cat /root/script/usb_mount_exception.list`
-BYID=`diff <($EXCEPTION) <(ls /dev/disk/by-id/ -l | sed -e "1d" | tr -s [:space:] | cut -d " " -f 9) | grep ^\> | cut -d " " -f 2`
-DEV=`ls -l /dev/disk/by-id/$BYID | grep .*sd[a-z][0-9] | sed 's/..\/..\///g' | awk '{print $NF}'`
+BYID=(`diff <($EXCEPTION) <(ls /dev/disk/by-id/ -l | sed -e "1d" | tr -s [:space:] | cut -d " " -f 9) | grep ^\> | cut -d " " -f 2 | tr "\n" " "`
+for item in ${BYID[@]}; do
+	DEV+=(`ls -l /dev/disk/by-id/$item | grep .*sd[a-z][0-9] | sed 's/..\/..\///g' | awk '{print $NF}'`)
+done
 FS=`lsblk -O | grep $DEV | tr -s [:space:] | cut -d " " -f 4`
 
+#https://qiita.com/exy81/items/723184c0fcd7953d0f2c
+#for((i=0; i<${#FilePath[@]}; i++))
+#do
+#    echo ${FilePath[i]}${vars[i]}
+#done
 echo "/dev/${DEV} /mnt/${MNT} ${FS} default 0 0" >> /etc/fstab
 mount -a
