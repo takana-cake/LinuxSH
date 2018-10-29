e59a28a8e3ab928e11888d31c2c57386011557a2
takana303 <msdf303@live.jp>
2018-10-30 05:32:28 +0900

    Update usb_mount.sh

--- usb_mount.sh
+++ usb_mount.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
-/dev/disk/by-id/*part1
-ここのやつを検知
+#/dev/disk/by-id/*part1
+#ここのやつを検知
+#
+#常設は例外リストに
+#usb_mount_exception.list
+#
+#swatchかなんかで監視して実行？
 
-常設は例外リストに
-usb_mount_exception.list
 
-
-
-ID=`diff <(cat /root/script/usb_mount_exception.list) <(ls /dev/disk/by-id/ -l | sed -e "1d" | tr -s [:space:] | cut -d " " -f 9) | grep ^\> | cut -d " " -f 2`
-DEV=`ls -l /dev/disk/by-id/$ID | sed 's/..\/..\///g' | awk '{print $NF}'`
+EXCEPTION=`cat /root/script/usb_mount_exception.list`
+BYID=`diff <($EXCEPTION) <(ls /dev/disk/by-id/ -l | sed -e "1d" | tr -s [:space:] | cut -d " " -f 9) | grep ^\> | cut -d " " -f 2`
+DEV=`ls -l /dev/disk/by-id/$BYID | grep .*sd[a-z][0-9] | sed 's/..\/..\///g' | awk '{print $NF}'`
 FS=`lsblk -O | grep $DEV | tr -s [:space:] | cut -d " " -f 4`
 
-
+echo "/dev/${DEV} /mnt/${MNT} ${FS} default 0 0" >> /etc/fstab
+mount -a
