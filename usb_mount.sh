6ac5b6c103dd08217ea906c55c608fbe3c27b1eb
takana303 <msdf303@live.jp>
2018-10-31 07:32:59 +0900

    Update usb_mount.sh

--- usb_mount.sh
+++ usb_mount.sh
@@ -11,7 +11,7 @@
 #ls /dev/disk/by-id/ -l | sed -e "1d" | tr -s [:space:] | cut -d " " -f 9 >> ./usb_mount_exception.list
 EXCEPTION=`cat ./usb_mount_exception.list`
 
-BYID=(`diff <($EXCEPTION) <(ls /dev/disk/by-id/ -l | sed -e "1d" | tr -s [:space:] | cut -d " " -f 9) | grep ^\> | cut -d " " -f 2 | tr "\n" " "`
+BYID=(`diff <($EXCEPTION) <(ls /dev/disk/by-id/ -l | sed -e "1d" | tr -s [:space:] | cut -d " " -f 9) | grep ^\> | cut -d " " -f 2 | tr "\n" " "`)
 for item in ${BYID[@]}; do
 	DEV=(`ls -l /dev/disk/by-id/$item | grep .*sd[a-z][0-9] | sed 's/..\/..\///g' | awk '{print $NF}'`)
 	FS=`lsblk -O | grep $DEV | tr -s [:space:] | cut -d " " -f 4`
@@ -24,4 +24,4 @@ done
 #	DEV+=(`ls -l /dev/disk/by-id/$BYID[i] | grep .*sd[a-z][0-9] | sed 's/..\/..\///g' | awk '{print $NF}'`)
 #	FS=`lsblk -O | grep $DEV[i] | tr -s [:space:] | cut -d " " -f 4`
 #done
-#mount -a
+mount -a
